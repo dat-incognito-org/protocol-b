b1bc8b50cf1087ee317077a99c0d044b2f869e26
darius <datle@incognito.org>
2022-10-11 12:25:22 +0700

    add params contract; stake by path

--- contracts/IMain.sol
+++ contracts/IMain.sol
@@ -2,16 +2,17 @@
 pragma solidity ^0.8.9;
 
 interface IMain {
-    function stake(MainStructs.NetworkRoles nwrole, uint amount, MainStructs.Networks[] calldata networks) external payable;
-    function unstake(MainStructs.NetworkRoles nwrole) external;
+    function stake(MainStructs.NetworkRoles nwrole, uint amount, address token, MainStructs.Networks[] calldata networks) external payable;
+    function unstake(MainStructs.NetworkRoles nwrole, address token) external;
     function swap(uint amount, address relayer, MainStructs.Networks dstNetwork, MainStructs.ExecutableMessage calldata srcMsg, MainStructs.ExecutableMessage calldata dstMsg) external payable;
-    function fulfill(MainStructs.SwapData calldata swapData, bytes calldata operatorAddr) external payable;
+    function fulfill(MainStructs.SwapData calldata swapData, bytes calldata boltOperatorAddr) external payable;
     function relay(MainStructs.SwapTranscript calldata swapData, bytes calldata signature, bool serving) external payable;
-    function relayReturn(bytes32 swapDataHash, address operator, bytes calldata signature) external;
+    function relayReturn(bytes32 swapDataHash, address boltOperator, bytes calldata signature) external;
     function slash(bytes[2] calldata signatures, MainStructs.SwapTranscript calldata ts) external;
+    function getEligibleRelayers(uint amount, address token) external view returns (MainStructs.BoltRelayer[] memory);
 
-    event Stake(address relayer, uint amount, MainStructs.Networks[] networks);
-    event Unstake(address relayer, uint amount, MainStructs.Networks[] networks);
+    event Stake(address relayer, uint amount, address token, MainStructs.Networks[] networks);
+    event Unstake(address relayer, uint amount, address token, MainStructs.Networks[] networks);
 
 }
 
@@ -19,12 +20,38 @@ abstract contract MainStructs {
     enum Networks {
         ETH,
         PLG,
-        BSC
+        BSC,
+        AVAX
     }
 
     enum NetworkRoles {
         SOURCE,
         DESTINATION
+    }
+
+    enum BoltActors {
+        RELAYER,
+        OPERATOR,
+        WATCHER
+    }
+
+    enum SlashRules {
+        RULE1,
+        RULE2,
+        RULE3,
+    }
+
+    enum Status {
+        INVALID,
+        WAITING,
+        FULFILLED,
+        COMPLETED,
+        REFUNDED
+    }
+
+    struct Route {
+        Networks src;
+        Networks dst;
     }
 
     struct ExecutableMessage {
@@ -37,30 +64,32 @@ abstract contract MainStructs {
     struct SwapData {
         uint srcAmount;
         uint crossAmount; // fees are functions of crossAmount & srcAmount
-        MainStructs.Networks srcNetwork;
-        MainStructs.Networks dstNetwork;
-        MainStructs.ExecutableMessage srcMsg;
-        MainStructs.ExecutableMessage dstMsg;
+        Route route;
+        ExecutableMessage srcMsg;
+        ExecutableMessage dstMsg;
+        Status status;
     }
 
-    // struct FulfillData {}
 
     struct SwapTranscript {
         SwapData swapData;
         address operator;
-        // bytes32 relayerRoot;
+    }
+
+    struct LockedFunds {
+        uint amount;
+        uint until;
+        bytes32 h; // the swap ID this is locked for
     }
 
     struct StakedFunds {
         uint stakedAmount;
-        uint lockedAmount; // TODO: add time as list
+        LockedFunds[] locked;
         Networks[] networks;
     }
 
-    struct Relayer {
-        StakedFunds to;
-        StakedFunds from;
+    struct BoltRelayer {
+        mapping (uint => StakedFunds) stake;
         bytes[] extraAddresses; // placeholder
     }
-
 }
--- contracts/Main.sol
+++ contracts/Main.sol
@@ -4,23 +4,36 @@ pragma solidity ^0.8.9;
 import "./IMain.sol";
 // import "hardhat/console.sol";
 
+/// @title Main logic contract for Bolt Protocol
 contract Main is IMain, MainStructs {
-    mapping (address => Relayer) public relayers;
+    Networks public immutable CURRENT_NETWORK;
+    address public constant NATIVE_TOKEN_ADDRESS = 0x0000000000000000000000000000000000000000;
+    address public immutable NATIVE_WRAPPED_TOKEN;
 
-    constructor() {
+    mapping (address => Relayer) public boltRelayers;
+    mapping (bytes32 => SwapData) public swaps;
+
+    constructor(address _wrapped, address _net) {
+        NATIVE_WRAPPED_TOKEN = _wrapped;
+        CURRENT_NETWORK = _net;
     }
 
-    function stake(NetworkRoles nwrole, uint amount, Networks[] calldata networks) external payable {
+    /// @notice Any relayer can add stake to take swap requests & earn fees;
+    /// @notice the stake is bound to one route, which consist of source & destination networks (stake amounts must be identical)
+    /// @param routes Routes to stake funds to e.g. [(ETH, PLG), (ETH, AVAX)]
+    /// @param amounts The amount to stake for each route
+    /// @param token The token to stake (zero for native coin)
+    function stake(Route[] routes, uint[] amounts, address token) external payable {
         StakedFunds storage sf = _stakedFundsStorage(nwrole);
         require(amount == msg.value, "stake ETH: amount invalid");
         require(sf.networks.length == 0 && sf.stakedAmount == 0, "add new only"); // TODO: add to existing
         sf.networks = networks;
         sf.stakedAmount = msg.value; // TODO: multiple tokens
 
-        emit Stake(msg.sender, amount, networks);
+        emit Stake(msg.sender, amount, token, networks);
     }
 
-    function unstake(NetworkRoles nwrole) external {
+    function unstake(Route r, address token) external {
         StakedFunds storage sf = _stakedFundsStorage(nwrole);
         require(sf.stakedAmount >= sf.lockedAmount, "invalid amounts");
         uint unstakeAmt = sf.stakedAmount - sf.lockedAmount;
@@ -28,12 +41,12 @@ contract Main is IMain, MainStructs {
 
         (bool success, ) = address(msg.sender).call{value: unstakeAmt}("");
         require(success, "unstake failed");
-        emit Unstake(msg.sender, unstakeAmt, sf.networks);
+        emit Unstake(msg.sender, unstakeAmt, token, sf.networks);
         sf.stakedAmount -= unstakeAmt;
     }
 
     function _stakedFundsStorage(NetworkRoles nwrole) internal view returns (StakedFunds storage sf) {
-        Relayer storage r = relayers[msg.sender];
+        Relayer storage r = boltRelayers[msg.sender];
         if (nwrole == NetworkRoles.SOURCE) {
             sf = r.to;
         } else {
@@ -42,10 +55,69 @@ contract Main is IMain, MainStructs {
         return sf;
     }
 
+    function routeId(Route r) public pure returns (uint) {
+        return r.src * 256 + r.dst;
+    }
+
+    modifier validRoute(Route r) {
+        require(r.src != r.dst, "ROUTE invalid");
+        require(r.src == CURRENT_NETWORK || r.dst == CURRENT_NETWORK, "route must contain current net");
+        _;
+    }
+
+    modifier validSrc(Route r) {
+        require(r.src != r.dst, "ROUTE invalid");
+        require(r.src == CURRENT_NETWORK, "SRC invalid");
+        _;
+    }
+
+    modifier validDst(Route r) {
+        require(r.src != r.dst, "ROUTE invalid");
+        require(r.dst == CURRENT_NETWORK, "DST invalid");
+        _;
+    }
+
     // TODO
-    function swap(uint amount, address relayer, MainStructs.Networks dstNetwork, MainStructs.ExecutableMessage calldata srcMsg, MainStructs.ExecutableMessage calldata dstMsg) external payable {}
-    function fulfill(MainStructs.SwapData calldata swapData, bytes calldata operatorAddr) external payable {}
+    function swap(uint amount, address boltRelayer, Networks dstNetwork, ExecutableMessage calldata srcMsg, ExecutableMessage calldata dstMsg) external payable {}
+    function fulfill(MainStructs.SwapData calldata swapData, bytes calldata boltOperatorAddr) external payable {}
     function relay(MainStructs.SwapTranscript calldata swapData, bytes calldata signature, bool serving) external payable {}
-    function relayReturn(bytes32 swapDataHash, address operator, bytes calldata signature) external {}
-    function slash(bytes[2] calldata signatures, MainStructs.SwapTranscript calldata ts) external {}
+    function relayReturn(bytes32 swapDataHash, address boltOperator, bytes calldata signature) external {}
+    function slash(bytes[2] calldata signatures, SwapTranscript calldata ts) external {}
+    function getAvailableRelayers(uint amount, address token) public view returns (Relayer[] memory r) {
+        return r;
+    }
+}
+
+contract BoltParameters is Ownable {
+    uint public constant BASE_PRECISION = 10000; // "percent" variables below are expressed in precision e.g. 2500 means 25%
+
+    uint[] public watcherRewardPercent; // reward percent varies per slash rule; there are 3 rules initially
+
+    uint[] public burnedCollateralPercent;
+
+    uint[] public minFee; // minFee is defined for each actor role
+    uint[] public feePercent; // minFee is defined for each actor role
+
+    // durations are in blocks (vary by network)
+    uint public durationToUnstake; // from stake accepted -> unstake enabled
+    uint public lockDuration; // time to lock a relayer's stake after a swap
+
+
+
+    constructor(uint[] memory _wreward, uint[] memory _burn, uint[] _minFee, uint[] _feePercent) {
+        watcherRewardPercent = _wreward;
+        burnedCollateralPercent = _burn;
+        minFee = _minFee;
+        feePercent = _feePercent;
+    }
+
+    // setters
+    function setWatcherRewardPercent(uint8 i, uint v) external onlyOwner {
+        require(i <= watcherRewardPercent.length, "watcherRewardPercent invalid index");
+        watcherRewardPercent[i] = v;
+    }
+
+    function setAllWatcherRewardPercent(uint[] lst) external onlyOwner {
+        watcherRewardPercent = lst;
+    }
 }
